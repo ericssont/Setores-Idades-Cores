ab960a8d3845380b556c851d4ca91499004dea3d
Ericsson Tribess <101569680+T-ribess@users.noreply.github.com>
2022-11-07 20:07:09 -0300

    Create setores.js

---
+++ setores.js
@@ -0,0 +1,11 @@
+    var idade = "";
+
+if (idade < 14) {
+    console.log("Setor amarelo");
+} else if (idade >14 < 18) {
+        console.log("Setor azul");
+} else if (idade > 18 < 25) {
+    console.log("Setor branco");
+} else {
+    console.log("Setor verde");
+}
